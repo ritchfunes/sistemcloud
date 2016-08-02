48a042f219143b522f844f33812e34a182b6a343
ritchfunes <ritchfunes@unitec.edu>
2016-08-01 19:05:54 -0600

    crud categoria

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802003211) do
+ActiveRecord::Schema.define(version: 20160802005752) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "nombre"
+    t.text     "descripcion"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
